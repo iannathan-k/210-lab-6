d0238e976718c5c78cfbc464bd7eb19649d0525e
iannathan-k <ianmtv@gmail.com>
2026-02-09 16:28:33 -0800

    update, finished

--- main.cpp
+++ main.cpp
@@ -6,34 +6,51 @@ using namespace std;
 
 const int SIZE = 5;
 
+void enterArrayData(double* ptr);
+
+void outputArrayData(double* ptr);
+
+double sumArray(double* ptr);
+
 int main() {
     double* array = new double[SIZE];
     enterArrayData(array);
     outputArrayData(array);
 
     double sum = sumArray(array);
-    cout << "Sum of values: " << sum;
+    cout << "Sum of values: " << sum << endl;
+
+    delete[] array;
 
     return 0;
 }
 
+// enterArrayData() populates each element through the console
+// arguments: double* ptr to the array
+// returns: none
 void enterArrayData(double* ptr) {
     cout << "Data entry for the array: " << endl;
     for (int i = 0; i < SIZE; i++) {
         cout << "\t> Element #" << i << ": ";
-        cin >> *(ptr + i);
+        cin >> *(ptr + i); // Using pointer notation method
     }
     cout << "Data entry complete." << endl;
 }
 
+// outputArrayData() outputs the array in a single line
+// arguments: double* ptr to the array
+// returns: none
 void outputArrayData(double* ptr) {
     cout << "Outputting array elements: ";
     for (int i = 0; i < SIZE; i++) {
-        cout << *(ptr + i);
+        cout << *(ptr + i) << " ";
     }
     cout << endl;
 }
 
+// sumArray() sums the array and returns the value
+// arguments: double* ptr to the array
+// returns double sum
 double sumArray(double* ptr) {
     double sum = 0;
     for (int i = 0; i < SIZE; i++) {
